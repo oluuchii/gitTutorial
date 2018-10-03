580d00bb18a03252e2194c6a377529f7bad62208
OLUCHI LILIAN ONUORAH <21709244@ad.ut-capitole.fr>
2018-10-03 11:37:35 +0200

    modified the window title

--- src/Application.java
+++ src/Application.java
@@ -18,7 +18,7 @@ public class Application extends JFrame {
 		View view = new View();
 		modl.addObserver(view);
 		view.addActionListener(ctrl);
-		this.setTitle("tutoriel git");
+		this.setTitle("counter window");
 		this.add(view);
 		this.pack();
 		this.setVisible(true);
